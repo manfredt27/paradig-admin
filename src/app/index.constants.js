3636807f51b78125e3ed9a33ddab89750acf5a80
Jonathan Avalos <javalomen@gmail.com>
2016-01-21 16:12:50 -0600

    Seed from generator

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('paradigAdmin')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('paradigAdmin', ['ngSanitize', 'ui.router', 'toastr']);
+
+})();
